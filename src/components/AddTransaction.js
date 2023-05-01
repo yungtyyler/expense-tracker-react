88ebdbed43a4ccc65be7f1716b100827da21b86e
Tyler Varzeas <tyler.varzeas@gmail.com>
2023-05-01 07:58:25 -0700

    Add components and styling for basic UI of application

---
+++ src/components/AddTransaction.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { useState } from 'react';
+
+export const AddTransaction = () => {
+    const [text, setText] = useState('');
+    const [amount, setAmount] = useState('0');
+
+  return (
+    <>
+        <h3>Add New Transaction</h3>
+        <form>
+            <div className='form-control'>
+                <label htmlFor='text'>Text</label>
+                <input type='text' value={text} onChange={(e) => setText(e.target.value)} placeholder='Enter Text...' />
+            </div>
+            <div className='form-control'>
+                <label htmlFor='amount'>Amount<br />(negative - expense, positive - income)</label>
+                <input type='number' value={amount} onChange={(e) => setAmount(e.target.value)} placeholder='Enter Amount...'/>
+            </div>
+            <button className='btn'>AddTransaction</button>
+        </form>
+    </>
+  );
+}
---
+++ src/components/TransactionList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const TransactionList = () => {
+  return (
+    <>
+        <h3>History</h3>
+        <ul className='list' id='list'>
+            <li className='minus'>Cash <span>-$400</span><button className='delete-btn'>x</button></li>
+        </ul>
+    </>
+  )
+}
